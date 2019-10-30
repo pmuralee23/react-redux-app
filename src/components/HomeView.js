842d4f7605c7f0a8e1c6b442f9ae04d361511d74
Murali Pulle - Vendor <Murali.Pulle@samsclub.com>
2019-10-30 09:44:11 -0500

    Updated HomeView

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -8,20 +8,28 @@ class HomeView extends React.Component {
                 <div className="step">Create React App: Npx create-react-app react-assignment</div>
                 <div className="step">Axios: Install axios for Backend API calls</div>
                 <div className="step">Pie Chart
-            <ul>
+                    <ul>
                         <li className="sub-step">Install react-chartjs-2 for Charts view</li>
                         <li className="sub-step">For customizing datalabels in the chart: npm install chartjs-plugin-datalabels –save</li>
                         <li className="sub-step">Color Schemas for Pie chart: npm install chartjs-plugin-colorschemes --save</li>
                     </ul>
                 </div>
                 <div className="step">React Bootstrap Table for Tabular ChartsView
-          <ul>
+                    <ul>
                         <li className="sub-step">npm install react-bootstrap-table --save</li>
                         <li className="sub-step">Added dummy event handler methods for handling different operations performed on data table</li>
                         <li className="sub-step">Have some commented code which is to customize the components</li>
                     </ul>
                 </div>
-                <div className="step"> Install redux, react-redux, redux-thunk
+                <div className="step"> <div>Install redux, react-redux, redux-thunkfor making Backend API calls.</div>
+                    <div>The flow with react-redux looks like this:
+                        <ul>
+                            <li>Create the Provider</li>
+                            <li>Pass it a reference to our Redux Store</li>
+                            <li>Wrap any component that needs to interact with the Store with Connect</li>
+                            <li>Connect passes down the different pieces of state and any action creators we need as props down to the wrapped component.</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
